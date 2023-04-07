c8ba6e4e875e73df677e170c04bf094712e998e6
yigitkarakurt <yigitkarakurt35@gmail.com>
2023-04-07 14:40:28 +0300

    Initial Commit

---
+++ MyChatSwiftUI-Firebase/MyChatSwiftUI_FirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyChatSwiftUI_FirebaseApp.swift
+//  MyChatSwiftUI-Firebase
+//
+//  Created by Yigit Karakurt on 7.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyChatSwiftUI_FirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
