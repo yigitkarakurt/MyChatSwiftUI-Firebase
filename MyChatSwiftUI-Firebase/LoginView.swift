e6692d52434da251a4d0f3a903340ae38670343f
yigitkarakurt <yigitkarakurt35@gmail.com>
2023-04-11 10:32:12 +0300

    firebase things
    profile photos, email and id added to firestore db

--- MyChatSwiftUI-Firebase/LoginView.swift
+++ MyChatSwiftUI-Firebase/LoginView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 import Firebase
 import FirebaseStorage
+import FirebaseFirestore
 
 class FirebaseManager: NSObject {
     
     let auth : Auth
     let storage : Storage
+    let firestore : Firestore
     
     static let shared = FirebaseManager()
     
@@ -21,6 +23,7 @@ class FirebaseManager: NSObject {
         
         self.auth = Auth.auth()
         self.storage = Storage.storage()
+        self.firestore = Firestore.firestore()
         super.init()
     }
 }
@@ -174,9 +177,30 @@ struct LoginView: View {
                 }
                 
                 self.loginStatusMessage = "Successfully stored image with url: \(url?.absoluteString ?? "")"
+                
+                guard let url = url else {
+                    return
+                }
+                self.storeUserInformation(imageProfileUrl : url)
             }
         }
         
+    }
+    
+    private func storeUserInformation(imageProfileUrl : URL){
+        
+        guard let uid = FirebaseManager.shared.auth.currentUser?.uid else {
+            return
+        }
+        let userData = ["email" : self.email, "uid" : uid, "profileImageUrl" : imageProfileUrl.absoluteString]
+        FirebaseManager.shared.firestore.collection("users")
+            .document(uid).setData(userData) { err in
+                if let err = err{
+                    self.loginStatusMessage = "\(err.localizedDescription)"
+                    return
+                }
+                self.loginStatusMessage = "Successfully created user"
+            }
     }
 }
 
